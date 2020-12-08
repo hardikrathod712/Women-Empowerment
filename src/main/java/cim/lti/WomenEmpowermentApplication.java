b537ad984424c4adc20c7d8d37f4473ab2094e61
Hardik <hardikrathod712.hr@gmail.com>
2020-12-08 10:57:45 +0530

    FAQ and Legislature Added

---
+++ src/main/java/cim/lti/WomenEmpowermentApplication.java
@@ -0,0 +1,13 @@
+package cim.lti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WomenEmpowermentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WomenEmpowermentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cim/lti/WomenEmpowermentApplicationTests.java
@@ -0,0 +1,13 @@
+package cim.lti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WomenEmpowermentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
